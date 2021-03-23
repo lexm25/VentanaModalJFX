3cbcfbaabc8e27644f52b445773126c6e3e72ace
Alejandro Martin <alejandromartinalvarez2000@gmail.com>
2021-03-23 13:15:46 +0100

    Subida VentanaModal

--- src/main/java/gui/viewsandcontrollers/main/MainController.java
+++ src/main/java/gui/viewsandcontrollers/main/MainController.java
@@ -1,8 +1,17 @@
 package gui.viewsandcontrollers.main;
 
+import java.io.IOException;
+
+import gui.viewsandcontrollers.segundaventana.SegundaVentanaController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MainController {
 
@@ -14,7 +23,15 @@ public class MainController {
     }
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        label.setText("Hola Mundo");
+    private void handleButtonAction(ActionEvent event) throws IOException {
+    	Stage stage = new Stage();
+        Parent root = FXMLLoader.load(
+            SegundaVentanaController.class.getResource("../segundaventana/SegundaVentana.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Ventana Modal");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(
+            ((Node)event.getSource()).getScene().getWindow() );
+        stage.show();
     }
 }
---
+++ src/main/java/gui/viewsandcontrollers/segundaventana/SegundaVentanaController.java
@@ -0,0 +1,5 @@
+package gui.viewsandcontrollers.segundaventana;
+
+public class SegundaVentanaController {
+
+}
